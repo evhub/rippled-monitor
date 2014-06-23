635fc583ec105e5f1b34260e9328db215560724a
evhub <evanjhub@gmail.com>
2014-06-23 10:16:35 -0700

    Moves binding to the create function

--- main.js
+++ main.js
@@ -2,28 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
                           var socketId;
                           var mainwin = document;
                           var log = mainwin.getElementById("log");
-
+                          
                           function display(item) {
                                 log.appendChild(mainwin.createTextNode(item+"\n"));
                           };
-
+                          
                           chrome.sockets.udp.create({}, function(socketInfo) {
+                                                    
+                                                    display("Connecting...");
+                                                    
                                                     socketId = socketInfo.socketId;
+                                                    
+                                                    chrome.sockets.udp.onReceive.addListener(function(info) {
+                                                                                             if (info.socketId !== socketId)
+                                                                                                return;
+                                                                                             display(info.data);
+                                                                                             });
+                                                    
+                                                    chrome.sockets.udp.bind(socketId, "0.0.0.0", 0, function(result) {
+                                                                            if (result < 0) {
+                                                                                display("Error binding socket.");
+                                                                                return;
+                                                                            });
+                                                    
+                                                    display("Connected.");
+                                                    
                                                     });
-
+                          
                           display("Log Initialized:");
-
-                          chrome.sockets.udp.onReceive.addListener(function(info) {
-                                                                   if (info.socketId !== socketId)
-                                                                        return;
-                                                                   display(info.data);
-                                                                   console.log(info.data);
-                                                                   });
-
-                          chrome.sockets.udp.bind(socketId, "0.0.0.0", 0, function(result) {
-                                                  if (result < 0) {
-                                                        console.log("Error binding socket.");
-                                                        return;
-                                                  });
-
+                          
                           });
